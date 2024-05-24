a12c2c91468340df78bb14b3413e8af907b00df1
Samuel Ryder <samuelryder@Samuels-MacBook-Pro.local>
2024-05-24 01:31:46 -0400

    moved root registration of app to separate file, made changes to babel.config.js enabling execution of jest react-native tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react-native";
 import { describe, expect, it } from "@jest/globals";
-
 import App from "../App";
 
 describe("App component", () => {
   it("renders App to screen", () => {
     render(<App />);
-    
+    expect(screen);
   });
 });
